64f26cd6e797dfec08ca70cfa63484e293621850
Jonas Renault <jonasrenault@gmail.com>
2023-12-14 20:33:45 +0100

    day19 part2

--- advent2020/day19.py
+++ advent2020/day19.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from utils.utils import Advent
 
 advent = Advent(19)
@@ -13,8 +15,96 @@ def main():
             c += 1
     advent.submit(1, c)
 
+    p42 = get_patterns(rules, "42")
+    p31 = get_patterns(rules, "31")
+    psize = len(p42[0])
 
-def matched(rules, rule: str, message: str):
+    c = 0
+    for m in messages:
+        if match_loop(psize, p31, p42, m):
+            c += 1
+    advent.submit(2, c)
+
+
+def match_loop(psize: int, p31: list[str], p42: list[str], message: str) -> bool:
+    """
+    rule 0: 8 11
+    rule 8: 42 | 42 8
+    rule 11: 42 31 | 42 11 31
+    This means a message matching rule 0 must match a pattern from 42 at least 2 times,
+    and finish by a pattern from 31 at least one times, with the added constraint that
+    there are more patterns from 42 than from 31
+    This function return True if the message matches this rule
+
+    Args:
+        psize (int): the size of the patterns
+        p31 (list[str]): the patterns for rule 31
+        p42 (list[str]): the patterns for rule 42
+        message (str): the message
+
+    Returns:
+        bool: True if message matches rule 0
+    """
+    if len(message) % psize != 0:
+        return False
+
+    seen42 = 0
+    seen31 = 0
+    for i in range(0, len(message), psize):
+        chunk = message[i : i + psize]
+        if seen42 < 2:
+            # start must be a 42 pattern
+            if chunk not in p42:
+                return False
+            seen42 += 1
+        elif seen31 == 0:
+            # if we've not seen a pattern from 31 yet,
+            # match a pattern from either rule
+            if chunk in p42:
+                seen42 += 1
+            elif chunk in p31:
+                seen31 += 1
+            else:
+                return False
+        else:
+            # once we've seen a 31 pattern, only match 31 patterns
+            if chunk in p31:
+                seen31 += 1
+            else:
+                return False
+
+    return seen42 > 1 and seen42 > seen31 > 0
+
+
+def get_patterns(rules: dict[str, list[list[str]]], rule: str) -> list[str]:
+    """
+    Return list of patterns matched by the given rule
+
+    Args:
+        rules (dict[str, list[list[str]]]): the rules
+        rule (str): the rule
+
+    Returns:
+        list[str]: the list of patterns that the rule matches
+    """
+    p = rules[rule]
+    if len(p) == 1 and not p[0][0].isdigit():
+        return p[0]
+
+    res = []
+    for part in p:
+        if len(part) == 1:
+            res.extend(get_patterns(rules, part[0]))
+        else:
+            l, r = part
+            l = get_patterns(rules, l)
+            r = get_patterns(rules, r)
+            res.extend([sub1 + sub2 for sub1, sub2 in product(l, r)])
+
+    return res
+
+
+def matched(rules: dict[str, list[list[str]]], rule: str, message: str) -> int:
     ors = rules[rule]
     if len(ors) == 1:
         ands = ors[0]
@@ -33,7 +123,7 @@ def matched(rules, rule: str, message: str):
         return -1
 
 
-def match_ands(rules, ands: list[str], message: str):
+def match_ands(rules: dict[str, list[list[str]]], ands: list[str], message: str) -> int:
     c = 0
     for r in ands:
         m = matched(rules, r, message[c:])
@@ -43,7 +133,7 @@ def match_ands(rules, ands: list[str], message: str):
     return c
 
 
-def parse(lines):
+def parse(lines: list[str]) -> tuple[dict[str, list[list[str]]], list[str]]:
     rules = {}
     messages = []
     for line in lines:
