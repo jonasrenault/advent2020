b64cd5c445280cff57a1beb61bdbac9d8b12e5bb
Jonas Renault <jonasrenault@gmail.com>
2024-02-23 13:43:57 +0100

    day20 part2 WIP

--- advent2020/day20.py
+++ advent2020/day20.py
@@ -3,6 +3,7 @@ import numpy as np
 import numpy.typing as npt
 from itertools import combinations
 from collections import defaultdict
+from collections.abc import Iterator
 from tqdm import tqdm
 from math import prod, sqrt
 from utils.algos import neighbors
@@ -18,128 +19,152 @@ def main():
     # corners have only 2 edges
     # advent.submit(1, prod([k for k, v in matching_tiles.items() if len(v) == 2]))
 
-    # # find each piece's place in the puzzle
-    # pieces = fit_pieces(pairs_match)
-    # print(pieces)
+    puzzle = build_puzzle(tiles, matching_tiles)
 
-    # # flip / rotate pieces to build the puzzle
-    # print(build_puzzle(tiles, pieces))
+    # trim edges and concatenate tiles
+    puzzle = [[c[1:-1, 1:-1] for c in r] for r in puzzle]
+    puzzle = [np.concatenate(r, axis=1) for r in puzzle]
+    image = np.concatenate(puzzle, axis=0)
+    for r in range(image.shape[0]):
+        print("".join(image[r, :].tolist()))
 
 
-def build_puzzle(tiles, pieces):
-    # place the first corner piece
-    corner = tiles[pieces[0, 0]]
-    # get the edges for the piece to its right
-    # pr_edges = set(get_edges(tiles[pieces[0, 1]])) | set(
-    #     get_edges(np.flip(tiles[pieces[0, 1]]))
-    # )
-    pr_edges = set(get_edges(tiles[pieces[0, 1]]))
-    # # do I need to flip it ?
-    # if not set(get_edges(corner)) & pr_edges:
-    #     corner = np.flip(corner)
+def build_puzzle(
+    tiles: dict[int, npt.NDArray[np.str_]],
+    matching_tiles: dict[int, list[int]],
+) -> list[list[npt.NDArray[np.str_]]]:
+    image_dimension = int(sqrt(len(tiles)))
 
+    # Get and orient top left corner
+    top_left_id = [k for k, v in matching_tiles.items() if len(v) == 2].pop()
+    top_left = tiles[top_left_id]
+
+    # turn / flip it until its matching edges are to the right and bottom
+    matching_edges = set(get_edges(tiles[matching_tiles[top_left_id][0]])) | set(
+        get_edges(tiles[matching_tiles[top_left_id][1]])
+    )
     while not (
-        np.array2string(corner[:, -1]) in pr_edges
-        or np.array2string(np.flip(corner[:, -1])) in pr_edges
+        get_edge(top_left, "R") in matching_edges
+        and get_edge(top_left, "B") in matching_edges
     ):
-        corner = np.rot90(corner)
+        top_left = np.rot90(top_left)
 
-    puzzle = []
-    row = [
-        corner,
-    ]
-    side = int(sqrt(len(tiles)))
-    flipud = False
-    for r in range(side):
-        if r != 0:
-            row = []
-        for c in range(side):
-            if r == 0 and c == 0:
-                continue
-            if c == 0:  # match to top
-                row.append(
-                    match_piece(
-                        tiles[pieces[r, c]], puzzle[r - 1][c], True, flipud and r < 2
-                    )
-                )
-            else:  # match to left
-                row.append(
-                    match_piece(tiles[pieces[r, c]], row[-1], False, flipud and r < 2)
-                )
+    to_place = set(tiles.keys()) - {top_left_id}  # set of remaining tiles to place
+    first = top_left
+    first_id = top_left_id
+    image = []
+    while to_place:
+        row = build_row(
+            first, first_id, tiles, matching_tiles, to_place, image_dimension
+        )
+        image.append(row)
 
-        if r == 0:  # should i flip top row ?
-            flipud = np.array2string(corner[0, :]) in set(
-                get_edges(tiles[pieces[1, 0]])
-            ) | set(get_edges(np.flip(tiles[pieces[1, 0]])))
-            if flipud:
-                row = [np.flipud(p) for p in row]
-        puzzle.append(row)
-    return puzzle
+        if to_place:  # if we still have tiles to place,
+            # match the first tile of the next row to the bottom of the
+            # first tile of current row
+            first_id, first = match_tile(
+                first, first_id, tiles, matching_tiles, to_place, "B", "T"
+            )
+            to_place.remove(first_id)
+
+    return image
 
 
-def match_piece(piece, other_piece, is_top, flipud):
-    # flip piece if it does not match other_piece edges
-    # if not set(get_edges(piece)) & set(get_edges(other_piece)):
-    #     piece = np.flip(piece)
-    if flipud:
-        piece = np.flipud(piece)
-
-    # rotate piece until top or left edge matches other_piece
-    if is_top:
-        while not get_edge(piece, "T") == get_edge(other_piece, "B"):
-            piece = np.rot90(piece)
-    else:
-        while not get_edge(piece, "L") == get_edge(other_piece, "R"):
-            piece = np.rot90(piece)
-
-    return piece
-
-
-def fit_pieces(pairs_match):
+def build_row(
+    first: npt.NDArray[np.str_],
+    first_id: int,
+    tiles: dict[int, npt.NDArray[np.str_]],
+    matching_tiles: dict[int, list[int]],
+    to_place: set[int],
+    image_dimension: int,
+) -> list[npt.NDArray[np.str_]]:
     """
-    Given list of matching pieces, place them next to each other
+    Build a row of dimension image_dimension by adding new tiles
+    to the right of first tile.
 
     Args:
-        pairs_match (_type_): _description_
+        first (npt.NDArray[np.str_]): the row's positionned first tile
+        first_id (int): the row's first tile id
+        tiles (dict[int, npt.NDArray[np.str_]]): dict of tiles
+        matching_tiles (dict[int, list[int]]): dict of matching tiles
+        to_place (set[int]): set of remaining tiles to place
+        image_dimension (int): row size
 
     Returns:
-        _type_: _description_
+        list[npt.NDArray[np.str_]]: the row of positioned tiles
     """
-    side = int(sqrt(len(pairs_match)))
-    puzzle = np.zeros((side, side), dtype=int)
-    for x in range(side):
-        for y in range(side):
-            find_piece(pairs_match, puzzle, x, y)
+    prev = first
+    prev_id = first_id
+    row = [first]
+    for _ in range(image_dimension - 1):
+        # add a new tile to the right
+        tile_id, tile = match_tile(
+            prev, prev_id, tiles, matching_tiles, to_place, "R", "L"
+        )
+        # remove added tile from available tiles
+        to_place.remove(tile_id)
+        row.append(tile)
+        prev = tile
+        prev_id = tile_id
 
-    return puzzle
+    return row
 
 
-def find_piece(pairs_match, puzzle, x, y):
-    side = int(sqrt(len(pairs_match))) - 1
-    pair_length = 4
-    if x in (0, side) and y in (0, side):  # corners
-        pair_length = 2
-    elif x in (0, side) or y in (0, side):  # sides
-        pair_length = 3
+def match_tile(
+    tile: npt.NDArray[np.str_],
+    tile_id: int,
+    tiles: dict[int, npt.NDArray[np.str_]],
+    matching_tiles: dict[int, list[int]],
+    to_place: set[int],
+    edge_a: str,
+    edge_b: str,
+) -> tuple[int, npt.NDArray[np.str_]]:
+    """
+    Given a positionned tile A, find another tile B in tile A's matching tiles
+    and rotate / flip it such that tile A and tile B match on edge_a and edge_b.
 
-    nbs = set([puzzle[n] for n in neighbors(puzzle, (x, y)) if puzzle[n] != 0])
+    Args:
+        tile (npt.NDArray[np.str_]): first tile
+        tile_id (int): first tile id
+        tiles (dict[int, npt.NDArray[np.str_]]): dict of tiles
+        matching_tiles (dict[int, list[int]]): dict of matching tiles
+        to_place (set[int]): set of remaining tiles to place
+        edge_a (str): matching edge for tile A
+        edge_b (str): matching edge for tile B
 
-    pieces = set(
-        [
-            k
-            for k, v in pairs_match.items()
-            if len(v) == pair_length and k not in puzzle and len(nbs.difference(v)) == 0
-        ]
-    )
-    puzzle[x, y] = pieces.pop()
+    Returns:
+        tuple[int, npt.NDArray[np.str_]]: matching tile id and tile
+    """
+    # get first tile's edge
+    side_a = get_edge(tile, edge_a)
+
+    # get set of possible matching tiles
+    other_tiles = set(matching_tiles[tile_id]) & to_place
+
+    # iterate over all possible matching tiles
+    for other_id in other_tiles:
+        other = tiles[other_id]
+        # Arrange second tile in any possible way
+        for other in arrangements(other):
+            # Until the two sides match
+            if side_a == get_edge(other, edge_b):
+                return other_id, other
 
 
 def get_matching_tiles(tiles: dict[int, npt.NDArray[np.str_]]) -> dict[int, list[int]]:
-    # find which tiles have matching edges
+    """
+    Find tiles with matching edges
+
+    Args:
+        tiles (dict[int, npt.NDArray[np.str_]]): dict of tile_id -> tile
+
+    Returns:
+        dict[int, list[int]]: dict of tile_id -> list of matching tile ids
+    """
     matching_tiles = defaultdict(list)
     for p1, p2 in tqdm(combinations(tiles.keys(), 2)):
-        ep1 = set(get_edges(tiles[p1])) | set(get_edges(np.flip(tiles[p1])))
-        ep2 = set(get_edges(tiles[p2])) | set(get_edges(np.flip(tiles[p2])))
+        ep1 = get_edges(tiles[p1])
+        ep2 = get_edges(tiles[p2])
         if ep1 & ep2:
             matching_tiles[p1].append(p2)
             matching_tiles[p2].append(p1)
@@ -147,17 +172,47 @@ def get_matching_tiles(tiles: dict[int, npt.NDArray[np.str_]]) -> dict[int, list
     return matching_tiles
 
 
-def get_edges(tile: npt.NDArray[np.str_]) -> list[str]:
-    edges = (
-        tile[0, :],  # top edge
-        tile[:, 0],  # left edge
-        tile[-1, :],  # bottom edge
-        tile[:, -1],  # right edge
-    )
-    return [np.array2string(e) for e in edges]
+def get_edges(tile: npt.NDArray[np.str_]) -> set[str]:
+    """
+    Get all possible edges for a tile by rotating and fliping it
+
+    Args:
+        tile (npt.NDArray[np.str_]): the tile
+
+    Returns:
+        set[str]: set of all possible edges
+    """
+    return set([np.array2string(r[0, :]) for r in arrangements(tile)])
+
+
+def arrangements(tile: npt.NDArray[np.str_]) -> Iterator[npt.NDArray[np.str_]]:
+    """
+    Rotate and flip tile to build all possible arrangements of a tile
+
+    Args:
+        tile (npt.NDArray[np.str_]): the tile
+
+    Yields:
+        Iterator[npt.NDArray[np.str_]]: iterator of possible arrangements
+    """
+    for _ in range(2):
+        for _ in range(4):
+            tile = np.rot90(tile)
+            yield tile
+        tile = np.flip(tile, 1)
 
 
 def get_edge(tile: npt.NDArray[np.str_], edge: str) -> str:
+    """
+    Get top, bottom, left or right edge of a tile
+
+    Args:
+        tile (npt.NDArray[np.str_]): the tile
+        edge (str): the edge
+
+    Returns:
+        str: the tile's edge
+    """
     if edge == "T":
         return np.array2string(tile[0, :])
     if edge == "L":
@@ -168,6 +223,15 @@ def get_edge(tile: npt.NDArray[np.str_], edge: str) -> str:
 
 
 def read_tiles(lines: list[str]) -> dict[int, npt.NDArray[np.str_]]:
+    """
+    Read tiles into a dict of tile_id -> tile
+
+    Args:
+        lines (list[str]): input lines
+
+    Returns:
+        dict[int, npt.NDArray[np.str_]]: dict of tiles
+    """
     id = None
     arr = []
     tiles = {}
